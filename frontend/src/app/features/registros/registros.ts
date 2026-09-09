d8721e3917abf8884492304c646c83626383fd60
amorales-2024057 <amorales-2024057@kinal.edu.gt>
2026-09-09 09:02:25 -0600

    fix: Se actualiza la rutas de movimientos en routes

--- frontend/src/app/features/registros/registros.ts
+++ frontend/src/app/features/registros/registros.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
-import { MovimientoService } from '../../../core/services/movimiento.service';
+import { MovimientoService } from '../../../core/service/movimiento.service';
 import {
     CategoriaIngreso,
     CrearMovimientoRequest,
